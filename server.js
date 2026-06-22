bad552650fea9d0e90779d29f03928c527e6d136
cartboostwidgets-debug <cartboostwidgets@gmail.com>
2026-06-22 12:11:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // OAuth callback de Tiendanube
 app.get('/oauth/callback', async (req, res) => {
@@ -51,7 +51,7 @@ app.get('/oauth/callback', async (req, res) => {
 
 // Servir index.html para cualquier otra ruta (SPA)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
